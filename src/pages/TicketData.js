4915db1bd168a18c20368dc792ca0065e1cc1f65
surbhi <surbhiagrawal786@gmail.com>
2023-07-11 21:45:27 +0530

    first

---
+++ src/pages/TicketData.js
@@ -0,0 +1,195 @@
+import { useCallback } from "react";
+import "./TicketData.css";
+const TicketData = () => {
+  const onITDashboardTextClick = useCallback(() => {
+    // Please sync "IT Desk Dashboard" to the project
+  }, []);
+
+  const onGroupContainer19Click = useCallback(() => {
+    // Please sync "IT Desk Dashboard" to the project
+  }, []);
+
+  return (
+    <div className="it-desk-dashboard">
+      <div className="it-desk-dashboard-child" />
+      <div className="it-desk-dashboard-item" />
+      <div className="it-desk-dashboard-item" />
+      <div className="it-desk-dashboard-item" />
+      <div className="it-desk-dashboard-item" />
+      <div className="it-desk-dashboard-item" />
+      <div className="group-parent">
+        <div className="rectangle-wrapper">
+          <div className="group-child" />
+        </div>
+        <div className="search-parent">
+          <div className="search">Search</div>
+          <img className="vector-icon" alt="" src="/vector.svg" />
+        </div>
+      </div>
+      <div className="pending-approval">Pending Approval</div>
+      <div className="it-desk-dashboard-child3" />
+      <div className="it-desk-dashboard-child3" />
+      <div className="user-profile-2">
+        <div className="rectangle-parent">
+          <div className="group-item" />
+          <img className="mask-group-icon" alt="" src="/vector.svg" />
+          <img className="mask-group-icon1" alt="" src="/vector.svg" />
+          <img className="group-inner" alt="" src="/vector.svg" />
+          <div className="ellipse-div" />
+          <img className="mask-group-icon2" alt="" src="/mask-group@2x.png" />
+          <img className="vector-icon1" alt="" src="/vector.svg" />
+          <div className="rectangle-div" />
+          <div className="group-child1" />
+          <div className="group-child2" />
+          <div className="role-parent">
+            <div className="role">Role</div>
+            <div className="uiux-designer">UI/UX Designer</div>
+          </div>
+          <div className="department-parent">
+            <div className="department">Department</div>
+            <div className="media">Media</div>
+          </div>
+          <div className="company-experience-parent">
+            <div className="company-experience">Company Experience</div>
+            <div className="month-12-day">1 Month, 12 Day</div>
+          </div>
+          <div className="group-child3" />
+          <div className="group-child4" />
+          <div className="group-child5" />
+          <img className="mask-group-icon3" alt="" src="/vector.svg" />
+          <img className="mask-group-icon4" alt="" src="/vector.svg" />
+          <img className="mask-group-icon5" alt="" src="/vector.svg" />
+          <img className="group-icon" alt="" src="/vector.svg" />
+          <img className="group-child6" alt="" src="/vector.svg" />
+          <img className="group-child7" alt="" src="/vector.svg" />
+          <div className="group-child8" />
+          <div className="group-child9" />
+          <div className="group-child10" />
+          <div className="designation-parent">
+            <div className="designation">Designation</div>
+            <div className="dy-gen-manage">Dy. Gen. Manage</div>
+          </div>
+          <div className="location-parent">
+            <div className="designation">Location</div>
+            <div className="dy-gen-manage">Bhopal</div>
+          </div>
+          <div className="overall-experience-parent">
+            <div className="overall-experience">Overall Experience</div>
+            <div className="years-6-months">7 Years, 6 Months</div>
+          </div>
+          <div className="group-child11" />
+          <div className="group-child12" />
+          <div className="group-child13" />
+          <img className="mask-group-icon6" alt="" src="/vector.svg" />
+          <img className="mask-group-icon7" alt="" src="/vector.svg" />
+          <img className="mask-group-icon8" alt="" src="/vector.svg" />
+          <img className="group-child14" alt="" src="/vector.svg" />
+          <img className="group-child15" alt="" src="/vector.svg" />
+          <img className="group-child16" alt="" src="/vector.svg" />
+          <div className="hey-jenny-10000004-parent">
+            <div className="hey-jenny-10000004-container">
+              <p className="hey">
+                <span>
+                  <span>Hey,</span>
+                </span>
+              </p>
+              <p className="jenny-10000004">
+                <span>
+                  <span>{`Jenny `}</span>
+                  <span className="span">(10000004)</span>
+                </span>
+              </p>
+            </div>
+            <div className="uiux-designer-parent">
+              <div className="uiux-designer1">
+                <span>UI</span>
+                <span className="span1">/</span>
+                <span>UX Designer</span>
+              </div>
+              <img className="group-child17" alt="" src="/vector.svg" />
+              <div className="group-child18" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <img className="it-desk-dashboard-child5" alt="" src="/vector.svg" />
+      <div className="group-container">
+        <img className="group-child19" alt="" src="/group-3473.svg" />
+        <div className="group-div">
+          <div className="dashboard-parent">
+            <div className="dashboard">{`Dashboard `}</div>
+            <img className="line-icon" alt="" src="/line-177.svg" />
+            <div className="itsm">ITSM</div>
+          </div>
+          <img className="group-child20" alt="" src="/group-18.svg" />
+        </div>
+        <img className="group-child21" alt="" src="/group-3476@2x.png" />
+      </div>
+      <div className="rectangle-group">
+        <div className="group-child22" />
+        <div className="rectangle-container">
+          <div className="group-child23" />
+          <div className="group-child24" />
+        </div>
+        <div className="rectangle-parent1">
+          <div className="group-child23" />
+          <div className="group-child24" />
+        </div>
+        <img className="group-child27" alt="" src="/group-814.svg" />
+        <div className="it-dashboard" onClick={onITDashboardTextClick}>
+          IT Dashboard
+        </div>
+        <img className="group-child28" alt="" src="/group-1485.svg" />
+        <div className="it-ticket">IT Ticket</div>
+      </div>
+      <div className="rectangle-parent2" onClick={onGroupContainer19Click}>
+        <div className="group-child29" />
+        <div className="raise-it-desk-ticket-parent">
+          <div className="raise-it-desk">Raise IT Desk Ticket</div>
+          <img className="group-child30" alt="" src="/group-762.svg" />
+        </div>
+      </div>
+      <div className="group-parent1">
+        <div className="rectangle-frame">
+          <div className="group-child31" />
+        </div>
+        <div className="rectangle-parent3">
+          <div className="group-child32" />
+          <div className="group-child33" />
+          <div className="group-child34" />
+          <div className="group-child35" />
+        </div>
+      </div>
+      <div className="my-requests">My Requests</div>
+      <div className="vector-parent">
+        <img className="group-child36" alt="" src="/vector-11.svg" />
+        <div className="search-group">
+          <div className="search1">Search</div>
+          <img className="vector-icon2" alt="" src="/vector1.svg" />
+          <div className="group-child37" />
+        </div>
+        <img className="vector-icon3" alt="" src="/vector2.svg" />
+      </div>
+      <img className="image-3-icon" alt="" src="/image-3@2x.png" />
+      <img className="image-5-icon" alt="" src="/image-5@2x.png" />
+      <img className="rectangle-icon" alt="" src="/rectangle-705.svg" />
+      <img
+        className="it-desk-dashboard-child6"
+        alt=""
+        src="/rectangle-702.svg"
+      />
+      <img
+        className="it-desk-dashboard-child7"
+        alt=""
+        src="/rectangle-704.svg"
+      />
+      <img
+        className="it-desk-dashboard-child8"
+        alt=""
+        src="/rectangle-703.svg"
+      />
+    </div>
+  );
+};
+
+export default TicketData;
